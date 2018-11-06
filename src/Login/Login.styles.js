238a9520bfa0c5aa4464bee3dc20b0e2f06f9cdd
Tad Scritchfield <tadscritch@gmail.com>
2018-11-05 18:03:18 -0700

    Add Login page

---
+++ src/Login/Login.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Page = styled.div`
+  display: flex;
+`
+
+export const Container = styled.div`
+  height: 300px;
+  width: 400px;
+  box-shadow: 0 0 10px 0 rgba(0,0,0,0.15)
+`
+
+export const Button = styled.button`
+  background-color: #123456
+`
