5233ff048a522b5bff58204e90c5f932dcd0d8bf
Tad Scritchfield <tadscritch@gmail.com>
2018-11-11 15:39:14 -0700

    Rename env vars

--- src/Pages/Login/Login.display.js
+++ src/Pages/Login/Login.display.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Page, Container, Link } from './Login.styles'
 
 export default () => {
-  const PC_CLIENT_ID = process.env.PC_CLIENT_ID
+  const PC_CLIENT_ID = process.env.REACT_APP_PC_CLIENT_ID
   const callback = process.env.NODE_ENV === 'production' ? `https://setlist.tadscritchfield.com/auth/success` : 'localhost:3000/auth/success'
   const loginUrl = `https://api.planningcenteronline.com/oauth/authorize?client_id=${PC_CLIENT_ID}&redirect_uri=${callback}&response_type=code&scope=people%20services`
   return (
