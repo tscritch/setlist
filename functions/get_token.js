9e5fa9848c3e84253e46efd7ec2a0b18f52238c1
Tad Scritchfield <tscritchfield@digitalairstrike.com>
2019-06-21 09:43:29 -0700

    function works! remove console logs

--- functions/get_token.js
+++ functions/get_token.js
@@ -1,8 +1,6 @@
 const request = require('request')
 
 exports.handler = function (event, context, callback) {
-  console.log(JSON.parse(event.body), '<- type of body')
-
   request(
     {
       url: 'https://api.planningcenteronline.com/oauth/token',
@@ -10,18 +8,15 @@ exports.handler = function (event, context, callback) {
       formData: JSON.parse(event.body)
     },
     function (err, response, body) {
-      console.log(body, '<- response body')
-      console.log(response.statusCode, '<- response body')
       if (!err && response.statusCode === 200) {
         callback(null, {
           statusCode: 200,
           body: JSON.stringify(body)
         })
       } else {
-        console.log(err, '<- error')
         callback(null, {
           statusCode: 500,
-          body: 'Something went wrong with the api request: ' + err
+          body: 'Something went wrong with the api request: ' + JSON.stringify(body)
         })
       }
     })
